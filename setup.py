344177715f83350705e5b60a7bd6dc2504a7ed2a
Jeff Terstriep <jefft@illinois.edu>
2018-03-08 06:12:51 +0000

    Add basic metadata to setup.py and create new description.rst file.

--- setup.py
+++ setup.py
@@ -1,6 +1,40 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+def description():
+    with open('description.rst') as f:
+         return f.read()
+
 
 setup(name='pyclowder',
       version='2.0.0',
-      packages=['pyclowder'],
+      packages=find_packages(),
+      description='TERRA-REF utility library',
+      long_description=description(),
+      #author='Max Burnette',
+      #author_email='mburnet2@illinois.edu',
+
+      url='clowder.ncsa.illinois.edu',
+      project_urls = {
+        'Source': 'https://opensource.ncsa.illinois.edu/bitbucket/scm/cats/pyclowder2.git',
+      },
+
+      license='BSD',
+      classifiers=[
+        'Development Status :: 5 - Production',
+        'Intended Audience :: Developers',
+        'Topic :: Data Science :: Data Management System',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7'
+      ],
+      keywords=['clowder', 'data management system'],
+
+      install_requires=[
+          'enum34',
+          'pika',
+          'PyYAML',
+          'requests',
+      ],
+
+      include_package_data=True,
+      zip_safe=True,
       )
