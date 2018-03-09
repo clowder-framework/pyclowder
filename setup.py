bc70974b3d0559353b2dd82d3f868a60eb0a6ac6
Jeff Terstriep <jefft@illinois.edu>
2018-03-09 23:43:33 +0000

    Change package description (it was for terrautils).

--- setup.py
+++ setup.py
@@ -11,21 +11,20 @@ def description():
 setup(name='pyclowder',
       version='2.0.0',
       packages=find_packages(),
-      description='TERRA-REF utility library',
+      description='Python SDK for the Clowder Data Management System',
       long_description=description(),
       author='Rob Kooper',
       author_email='kooper@illinois.edu',
 
-      url='clowder.ncsa.illinois.edu',
+      url='https://clowder.ncsa.illinois.edu',
       project_urls={
         'Source': 'https://opensource.ncsa.illinois.edu/bitbucket/scm/cats/pyclowder2.git',
       },
 
       license='BSD',
       classifiers=[
-        'Development Status :: 5 - Production',
-        'Intended Audience :: Developers',
-        'Topic :: Data Science :: Data Management System',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7'
       ],
