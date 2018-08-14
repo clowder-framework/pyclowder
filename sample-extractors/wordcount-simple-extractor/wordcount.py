60c7debe2a94fee8e3b07f3c9f5b6b813b2f741a
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2018-08-14 10:45:40 -0500

    Updated word count simple extractor with more comments. Renamed the function variable.

--- sample-extractors/wordcount-simple-extractor/wordcount.py
+++ sample-extractors/wordcount-simple-extractor/wordcount.py
@@ -1,15 +1,31 @@
 import subprocess
 
 
-def wordcount(input_file):
-    result = subprocess.check_output(['wc', input_file], stderr=subprocess.STDOUT)
+def wordcount(input_file_path):
+    """
+    This function calculates the number of lines, words, and characters in a text format file.
+
+    :param input_file_path: Full path to the input file
+    :return: Result dictionary containing metadata about lines, words, and characters in the input file
+    """
+
+    # Execute word count command on the input file and obtain the output
+    result = subprocess.check_output(['wc', input_file_path], stderr=subprocess.STDOUT)
+
+    # Split the output string into lines, words, and characters
     (lines, words, characters, _) = result.split()
+
+    # Create metadata dictionary
     metadata = {
         'lines': lines,
         'words': words,
         'characters': characters
     }
+
+    # Store metadata in result dictionary
     result = {
         'metadata': metadata
     }
+
+    # Return the result dictionary
     return result
