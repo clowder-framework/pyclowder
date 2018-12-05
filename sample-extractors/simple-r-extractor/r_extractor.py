2ebddbb5b9dba23d259c62b1322a8c91f7ac8451
Rob Kooper <kooper@illinois.edu>
2018-12-04 23:15:37 -0600

    use launcher script
    Original version use python module to call R code. This would lock all python threads resulting in missing heartbeats.

--- sample-extractors/simple-r-extractor/r_extractor.py
+++ sample-extractors/simple-r-extractor/r_extractor.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python
 
 import json
-import os
+import subprocess
+import tempfile
 
 from pyclowder.extractors import SimpleExtractor
-import rpy2.robjects as robjects
-
-r_script = os.getenv("R_SCRIPT")
-r_function = os.getenv("R_FUNCTION")
 
 
 class RExtractor(SimpleExtractor):
     def process_file(self, input_file):
-        r_result = robjects.r('''
-                    if ("%s" != "") {
-                        source("%s")
-                    }
-                    result <- do.call("%s", list("%s"))
-                    jsonlite::toJSON(result, auto_unbox=TRUE)
-                ''' % (r_script, r_script, r_function, input_file))
-        return json.loads(str(r_result))
+        with tempfile.NamedTemporaryFile(suffix=".json") as json_file:
+            subprocess.check_call(['/home/clowder/launcher.R', input_file, json_file.name])
+            return json.load(json_file.file)
 
 
 RExtractor().start()
