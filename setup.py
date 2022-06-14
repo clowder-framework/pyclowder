c5b4aa4ace4f173262a18cddee4f21748452b134
Rob Kooper <kooper@illinois.edu>
2022-06-14 14:46:07 -0500

    fix #47 now the name = queue name

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / 'description.rst').read_text(encoding='utf-8')
 
 setup(
     name='pyclowder',
-    version='2.5.1',
+    version='2.6.0',
     description='Python SDK for the Clowder Data Management System',
     long_description=long_description,
 
