a5486c1be5772727fa83ab99ba5b9111a158d67b
toddn <tcnichol@illinois.edu>
2024-03-30 12:52:12 -0500

    the backend route returns 2 fields: metadata and data. The metadata is related to the pagination, and the data is the actual list of files. We want to get the list of files here only, or else other parts of the code will break.

--- pyclowder/datasets.py
+++ pyclowder/datasets.py
@@ -128,7 +128,7 @@ def get_file_list(connector, host, key, datasetid):
     datasetid -- the dataset to get filelist of
     """
     client = ClowderClient(host=host, key=key)
-    file_list = datasets.get_file_list(connector, client, datasetid)
+    file_list = datasets.get_file_list(connector, client, datasetid)['data']
     return file_list
 
 
